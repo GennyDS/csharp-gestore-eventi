b9b42ba64005f41bd016a7512359fbbaed35cf9f
Gennaro <gennarinodi02@gmail.com>
2022-04-22 13:02:24 +0200

    2commit

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -8,5 +8,172 @@ namespace GestoreEventi
 {
     internal class Evento
     {
+        private string Titolo;
+        private DateTime Data;
+        private int CapienzaMaxEvento;
+        private int nPostiPrenotati;
+
+        public Evento(string Titolo, DateTime Data, int CapienzaMaxEvento, int nPostiPrenotati)   //Costruttore
+        {
+            this.Titolo = Titolo;
+            this.Data = Data = DateTime.Now; ;
+           ;
+            this.nPostiPrenotati = nPostiPrenotati=0;
+
+        }
+
+
+        public string GetTitolo()
+        {
+            return Titolo;
+        }
+        public string ModificaTitolo()
+        {
+            bool flag = false;
+            string NuovoTitolo="";
+            do 
+            {
+
+               Console.WriteLine("Inserisci un nuovo titolo");
+                NuovoTitolo=Console.ReadLine();
+                 if(NuovoTitolo==null)
+                 {
+                         throw new Exception("il titolo non può essere vuoto");
+                 }
+                else
+                {
+                    flag=true;
+                }
+            }while(flag==false);
+
+            return NuovoTitolo;
+        }
+
+
+        public DateTime GetData()
+        {
+            return Data;
+        }
+
+
+        /*
+        public DateTime ModificaData()
+        {
+            DateTime NuovaData=;
+            int flag=0;
+            while (flag==0)
+            {
+
+
+                Console.WriteLine("Inserisci una nuova data");
+                NuovaData = DateTime.Parse(Console.ReadLine());
+
+                if (NuovaData < Data)
+                {
+                    throw new Exception("La Data è Gia Passata,riprova");
+                }
+                else if ((NuovaData > Data) || (NuovaData == Data))
+                {
+                    
+                    flag =1;
+               
+                }
+                return NuovaData;
+            }
+           
+
+
+        }
+        */
+
+
+        public int  GetCapienzaMaxEvento()
+        {
+            return CapienzaMaxEvento;
+        }
+
+
+        public int  GetNPostiPrenotati()
+        {
+            return nPostiPrenotati;
+        }
+
+
+
+
+        public int  PrenotaPosti(int nPostiMaxUtente)
+        {
+            int NuoviPostiPrenotati;
+            bool flag = false;
+
+            do
+            {
+
+                Console.WriteLine("Inserisci il numero di posti che vuoi prenotare");
+                NuoviPostiPrenotati = int.Parse(Console.ReadLine());
+
+                NuoviPostiPrenotati += nPostiPrenotati;
+
+                if (NuoviPostiPrenotati >nPostiMaxUtente)
+                {
+                    throw new Exception("Posti non disponibili,riprova");
+                }
+                else
+                {
+                    flag = true;
+                }
+            } while (flag == false);
+
+                return NuoviPostiPrenotati;
+        }
+
+
+
+        public int DisdiciPosti( int nPostiRimossi)
+        {
+
+            bool flag = false;
+
+            while (flag == false)
+            {
+
+                 Console.WriteLine("Inserisci il numero di posti che vuoi disdire");
+                 nPostiRimossi= int.Parse(Console.ReadLine());
+                nPostiRimossi -= nPostiPrenotati;
+
+                if(nPostiRimossi>nPostiPrenotati)
+                {
+                    throw new Exception("il numero dei posti rimossi è maggiore dei posti prenotati");
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+            return nPostiRimossi;
+        }
+
+        public virtual string ToString()
+        {
+            string rappresentazioneInStringa = "";
+
+            rappresentazioneInStringa += "---- Evento ---- \n";
+            rappresentazioneInStringa += this.Data + "--"+this.Titolo;
+            
+
+            return rappresentazioneInStringa;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,1 +1,74 @@
 ﻿using GestoreEventi;
+
+List<Evento> ListaDiEventi= new List<Evento>();
+
+Console.WriteLine("inserisci il Titolo dell'evento ");
+string TitoloUtente=Console.ReadLine();
+
+
+Console.WriteLine("inserisci la Data dell'evento ");
+DateTime DataUtente=DateTime.Parse(Console.ReadLine());
+
+Console.WriteLine("inserisci il Numero dei posti Massimi dell'evento ");
+int nPostiMaxUtente = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("inserisci il numero dei posti che intendi prenotare  ");
+ int nPostiPrenotatiUtente=int.Parse(Console.ReadLine());
+
+
+
+int nPostiDisponibiliUtente = nPostiMaxUtente - nPostiPrenotatiUtente;
+
+//-------------CONTROLLO POSTI---------
+if(nPostiDisponibiliUtente <0)
+{
+    throw new ArgumentOutOfRangeException("Numero negativo");
+}
+
+
+
+
+//-------CREZIONE OGGETTO------
+Evento mioEvento = new Evento(TitoloUtente, DataUtente, nPostiMaxUtente, nPostiPrenotatiUtente);
+
+
+Console.WriteLine("Numero di posti prenotati: "+nPostiPrenotatiUtente);
+Console.WriteLine("Numero di posti Disponibili: " +nPostiDisponibiliUtente);
+
+//-----------------------------------------------
+
+//-------CONTROLLO RISPOSTA UTENTE---------
+bool flag = false;
+
+while (flag == false)
+{
+
+    Console.WriteLine("Vuoi Prenotare altri Posti ? (Si/No)");
+    string RispostaUtente = Console.ReadLine();
+    RispostaUtente = RispostaUtente.ToLower();
+
+    if (RispostaUtente == "si")
+    {
+        int nNuoviPostiPrenotati = mioEvento.PrenotaPosti(nPostiMaxUtente);
+
+        nNuoviPostiPrenotati += nPostiPrenotatiUtente;
+        int nNuoviPostiDisponibili = nPostiMaxUtente - nNuoviPostiPrenotati;
+
+        Console.WriteLine("Numero di posti prenotati: " + nNuoviPostiPrenotati);
+        Console.WriteLine("Numero di posti Disponibili: " + nNuoviPostiDisponibili);               //EROORE SULL'INSERIMENTO DEI NUMERI DOPO IL SECONDO SI (deve essere obbligatoriaamnete  più grande)
+
+    }
+    else if (RispostaUtente == "no")
+    {
+        Console.WriteLine("Ok VaBene!!");
+        flag=true;
+
+    }
+    else
+    {
+        throw new ArgumentOutOfRangeException("Hai inserito una risposta non contemplata,RIPROVA");
+    }
+}
+
+
