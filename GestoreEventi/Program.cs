da81ffa2c3a51f1dff2ca0410b30e36cefe78a52
Gennaro <gennarinodi02@gmail.com>
2022-04-22 17:40:15 +0200

    commit

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -81,7 +81,7 @@ Console.WriteLine("Vuoi DISDIRE i posti ? (Si/No)");
        
 
        Console.WriteLine("Numero di posti prenotati: " + mioEvento.GetNPostiPrenotati());
-        Console.WriteLine("Numero di posti Disponibili: " + mioEvento.GetPostiDisponibili(nPostiDisdetti));
+        Console.WriteLine("Numero di posti Disponibili: " + mioEvento.GetPostiDisponibili(nPostiDisdetti));  //non stampa 
 
     }
         else if (RispostaUtenteDisdire =="no")
